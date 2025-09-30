d3f7e0848d2d926c355260818ac0319f43b00ec2
samueldamasceno <sdamasceno756@gmail.com>
2025-09-30 20:00:09 -0300

    refactor(css): mudança sutil na estilização da página de login/cadastro

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -12,13 +12,14 @@ import { useNavigation } from "@react-navigation/native";
 import { auth } from "../../firebase/firebaseConfig";
 import { useTheme } from "../../theme";
 import Screen from "../../components/common/Screen";
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { AuthStackParamList } from "../../navigation/AuthStack";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const navigation = useNavigation<NativeStackNavigationProp<AuthStackParamList>>();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<AuthStackParamList>>();
   const { colors } = useTheme();
 
   const handleLogin = async () => {
@@ -31,46 +32,49 @@ export default function LoginScreen() {
 
   return (
     <Screen backgroundColor={colors.bg} padded>
-      <Text style={[styles.title, { color: colors.primary }]}>Entrar</Text>
-      <TextInput
-        style={[
-          styles.input,
-          { backgroundColor: colors.bgSecundary, color: colors.text },
-        ]}
-        placeholder="Email"
-        placeholderTextColor={colors.placeholder}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={[
-          styles.input,
-          { backgroundColor: colors.bgSecundary, color: colors.text },
-        ]}
-        placeholder="Senha"
-        placeholderTextColor={colors.placeholder}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <TouchableOpacity
-        style={[styles.button, { backgroundColor: colors.primary }]}
-        onPress={handleLogin}
-      >
-        <Text style={[styles.buttonText, { color: colors.bg }]}>Entrar</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-        <Text style={{ color: colors.primary, marginTop: 12 }}>
-          Criar uma conta
-        </Text>
-      </TouchableOpacity>
+      <View style={styles.center}>
+        <Text style={[styles.title, { color: colors.primary }]}>Entrar</Text>
+        <TextInput
+          style={[
+            styles.input,
+            { backgroundColor: colors.bgSecundary, color: colors.text },
+          ]}
+          placeholder="Email"
+          placeholderTextColor={colors.placeholder}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={[
+            styles.input,
+            { backgroundColor: colors.bgSecundary, color: colors.text },
+          ]}
+          placeholder="Senha"
+          placeholderTextColor={colors.placeholder}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: colors.primary }]}
+          onPress={handleLogin}
+        >
+          <Text style={[styles.buttonText, { color: colors.bg }]}>Entrar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+          <Text style={{ color: colors.primary, marginTop: 12 }}>
+            Criar uma conta
+          </Text>
+        </TouchableOpacity>
+      </View>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
   title: { fontSize: 24, fontWeight: "800", marginBottom: 16 },
-  input: { borderRadius: 12, padding: 12, marginBottom: 12, fontSize: 16 },
+  input: { borderRadius: 12, padding: 12, marginBottom: 12, fontSize: 16, width: "100%" },
   button: { borderRadius: 12, padding: 14, alignItems: "center", marginTop: 8 },
   buttonText: { fontSize: 16, fontWeight: "600" },
+  center: { flex: 1, justifyContent: "center", alignItems: "center", width: "100%", paddingHorizontal: 32},
 });
--- src/screens/auth/RegisterScreen.tsx
+++ src/screens/auth/RegisterScreen.tsx
@@ -33,55 +33,62 @@ export default function RegisterScreen() {
 
   return (
     <Screen backgroundColor={colors.bg} padded>
-      <Text style={[styles.title, { color: colors.primary }]}>Criar Conta</Text>
-      <TextInput
-        style={[
-          styles.input,
-          { backgroundColor: colors.bgSecundary, color: colors.text },
-        ]}
-        placeholder="Nome"
-        placeholderTextColor={colors.placeholder}
-        onChangeText={setName}
-      />
-      <TextInput
-        style={[
-          styles.input,
-          { backgroundColor: colors.bgSecundary, color: colors.text },
-        ]}
-        placeholder="Email"
-        placeholderTextColor={colors.placeholder}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={[
-          styles.input,
-          { backgroundColor: colors.bgSecundary, color: colors.text },
-        ]}
-        placeholder="Senha"
-        placeholderTextColor={colors.placeholder}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <TouchableOpacity
-        style={[styles.button, { backgroundColor: colors.primary }]}
-        onPress={handleRegister}
-      >
-        <Text style={[styles.buttonText, { color: colors.bg }]}>Cadastrar</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text style={{ color: colors.primary, marginTop: 12 }}>
-          Voltar para login
+      <View style={styles.center}>
+        <Text style={[styles.title, { color: colors.primary }]}>
+          Criar Conta
         </Text>
-      </TouchableOpacity>
+        <TextInput
+          style={[
+            styles.input,
+            { backgroundColor: colors.bgSecundary, color: colors.text },
+          ]}
+          placeholder="Nome"
+          placeholderTextColor={colors.placeholder}
+          onChangeText={setName}
+        />
+        <TextInput
+          style={[
+            styles.input,
+            { backgroundColor: colors.bgSecundary, color: colors.text },
+          ]}
+          placeholder="Email"
+          placeholderTextColor={colors.placeholder}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={[
+            styles.input,
+            { backgroundColor: colors.bgSecundary, color: colors.text },
+          ]}
+          placeholder="Senha"
+          placeholderTextColor={colors.placeholder}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: colors.primary }]}
+          onPress={handleRegister}
+        >
+          <Text style={[styles.buttonText, { color: colors.bg }]}>
+            Cadastrar
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text style={{ color: colors.primary, marginTop: 12 }}>
+            Voltar para login
+          </Text>
+        </TouchableOpacity>
+      </View>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
   title: { fontSize: 24, fontWeight: "800", marginBottom: 16 },
-  input: { borderRadius: 12, padding: 12, marginBottom: 12, fontSize: 16 },
+  input: { borderRadius: 12, padding: 12, marginBottom: 12, fontSize: 16, width: "100%" },
   button: { borderRadius: 12, padding: 14, alignItems: "center", marginTop: 8 },
   buttonText: { fontSize: 16, fontWeight: "600" },
+  center: { flex: 1, justifyContent: "center", alignItems: "center", width: "100%", paddingHorizontal: 32},
 });
